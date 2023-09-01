406ad4228b9607bc5f2b0c8d5573e7eed58916d7
beckjosh <Joshbeck17@gmail.com>
2023-09-01 18:06:42 +0200

    Heaps of updates and hooks

--- src/gameHelpers.ts
+++ src/gameHelpers.ts
@@ -11,3 +11,29 @@ export const randomTetromino = () => {
   return TETROMINOS[randTetromino];
 };
 
+export const isColliding = (
+  player: PLAYER,
+  stage: STAGE,
+  { x: moveX, y: moveY }: { x: number, y: number } 
+) => {
+  // Using for loops to be able to return and break
+  for (let y = 0; y < player.tetromino.length; y +=1) {
+    for (let x = 0; x < player.tetromino[y].length; x += 1) {
+      // 1. Check that are on an actual tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        if (
+          // 2. Check that our move is within the game area height (Y) and not passing through the bottom
+          !stage[y + player.pos.y + moveY] ||
+          // 3. Check that our move is within the game width (x)
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          // 4. Check that that the cell we are moving to isnt clear
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== "clear"
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  // /if everything above is falsae, then return false
+  return false;
+}
--- src/hooks/usePlayer.ts
+++ src/hooks/usePlayer.ts
@@ -42,4 +42,24 @@ export const usePlayer = () => {
     }
     setPlayer(clonedPlayer);
   }
+
+  const updatePlayerPos = ({ x, y, collided }: { x:number, y: number, collided: boolean }): void => {
+    setPlayer(prev => ({
+      ...prev,
+      post: { x: prev.pos.x += x, y: prev.pos.y += y },
+      collided
+    }));
+  };
+
+  const resetPlayer = React.useCallback(
+    (): void => 
+    setPlayer({
+      pos: { x: STAGE_WIDTH / 2 -2, y: 0 },
+      tetromino: randomTetromino().shape,
+    collided:false
+      }),
+      []
+  );
+
+  return { player, updatePlayerPos, resetPlayer, playerRotate };
 }
